2e00fe68ec4b3edcf32a8cbd2e9ddff7c0d4e5bc
Talha Balaj <talhabalaj@hotmail.com>
2018-12-14 18:06:14 +0500

    bhai or hal kar diyay hian. cheating nahi karni

--- PPS/PPS.cpp
+++ PPS/PPS.cpp
@@ -1,6 +1,15 @@
+
+/* 
+	Any suggestions are welcome. Please contact me if you wanna
+	improve this solution. 
+
+	Problems and bugs are expected. Do contact me if you find 
+	some of them
+*/
+
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 // #1  
 void cookTea() {
@@ -17,8 +26,36 @@ void addTwoNumbers() {
 	cout << "Sum: " << sum;
 }
 // #3
+void getQuotient() {
+	int num1, num2;
+	cout << "Enter num1: ";
+	cin >> num1; 
+	cout << "Enter num2: ";
+	cin >> num2;
+	if (num2 != 0) {
+		cout << num1 / num2;
+	}
+	else {
+		cout << "num2 can't be zero";
+	}
+}
 // #4
+void generateSumOfFourNumbers() {
+	int n1, n2, n3, n4, sum;
+	cout << "Enter 4 numbers: ";
+	cin >> n1 >> n2 >> n3 >> n4;
+	sum = n1 + n2 + n3 + n4;
+	cout << "Sum: " << sum;
+} 
 // #5
+void sumAndAverageOfMarks() {
+	int s1, s2, s3, s4, s5, sum, avg;
+	cout << "Enter marks of students: ";
+	cin >> s1 >> s2 >> s3 >> s4 >> s5;
+	sum = s1 + s2 + s3 + s4 + s5;
+	avg = sum / 5;
+	cout << "Sum: " << sum << " Average: " << avg;
+}
 // #6
 void findPercentageOfFourStudents() {
 	int oMarks1, oMarks2, oMarks3, oMarks4, totalMarks;
@@ -37,8 +74,45 @@ void findPercentageOfFourStudents() {
 	cout << "Percentage: " << percentage;
 }
 // #7
+void sayGreatIfGreaterThan80() {
+	int aNumber;
+	cout << "Enter a number: ";
+	cin >> aNumber; 
+	if (aNumber > 80) cout << "good";
+	else cout << "Try again";
+}
 // #8
+void isDivisableOrNot() {
+	int num1, num2;
+	cout << "Enter num1 and num2: ";
+	cin >> num1 >> num2;
+	if (num2 != 0) {
+		if (num1 % num2 == 0) {
+			cout << "Divisable";
+		}
+		else {
+			cout << "Not divisable";
+		}
+	}
+	else {
+		cout << "illegal Operation";
+	}
+}
 // #9
+void sumOf10UserGivenEvenNumbers() {
+	int sum = 0;
+	int n = 10;
+	int number;
+	while (n >= 1) {
+		cout << "Enter a number: ";
+		cin >> number;
+		if (number % 2 == 0) {
+			sum += number;
+		}
+		n--;
+	}
+	cout << "Sum of Even Numbers: " << sum;
+}
 // #10
 void sumOfEvenNumbersFromUser() {
 	int sum = 0;
@@ -57,6 +131,18 @@ void sumOfEvenNumbersFromUser() {
 	cout << "Sum of Even Numbers: " << sum;
 }
 // #11
+void printFibonacciSeries() {
+	int prev1 = 0, prev2 = 1, current = 0, limit;
+	cout << "Enter limit: ";
+	cin >> limit; 
+	while (limit > 0) {
+		cout << current << endl;
+		prev1 = prev2;
+		prev2 = current;
+		current = prev1 + prev2;
+		limit--;
+	}
+}
 // #12
 void tempFtoC() {
 	int tempInF, tempInC;
@@ -66,8 +152,51 @@ void tempFtoC() {
 	cout << "Temperature in C: " << tempInC;
 }
 // #13
+void calculateThePay() {
+	int payRate, hoursWorked;
+	cout << "Hours Worked: ";
+	cin >> hoursWorked;
+	cout << "Payrate: ";
+	cin >> payRate;
+	cout << "Pay: " << hoursWorked * payRate;
+}
 // #14
+void DidShePass() {
+	int marks;
+	cout << "Enter marks: ";
+	cin >> marks;
+	if (marks >= 50) cout << "She passed bruh, you got problem?";
+	else cout << "She failed :( better next time";
+}
 // #15
+void howMuchtoPayThisPoorMan() {
+	int payRate, hoursWorked, regularPay;
+	cout << "How many hours did you grind"
+		<< endl
+		<< "> ";
+	cin >> hoursWorked;
+	cout << "How much per hour that fat boi give ya?"
+		<< endl
+		<< "Rs. ";
+	cin >> payRate;
+	if (hoursWorked > 40) {
+		regularPay = 40 * payRate;
+		int overTime = hoursWorked - 40;
+		double overTimePay = overTime * 1.5 * payRate;
+		cout << "That fat boi needs to give ya Rs."
+<< regularPay
+<< " with overtime pay of Rs."
+<< overTimePay
+<< " Total of Rs. "
+<< overTimePay + regularPay;
+	}
+	else {
+	regularPay = hoursWorked * payRate;
+	cout << "Voila, you need to be paid Rs."
+		<< regularPay
+		<< " by that fat boi. ";
+	}
+}
 // #16
 void greaterInTwo() {
 	int num1, num2;
@@ -79,13 +208,91 @@ void greaterInTwo() {
 	if (num1 > num2) {
 		cout << "num1 is greater than num2";
 	}
-	 else {
-	  cout << "num2 is greater than num1";
+	else {
+		cout << "num2 is greater than num1";
 	}
 }
-// #17
+// #17 a)
+void greatestIn3() {
+	int a, b, c;
+	cout << "Enter a b c: ";
+	cin >> a >> b >> c;
+	if (a > b && a > c)
+		cout << "a is greatest bud! :D ";
+	else if (b > a && b > c)
+		cout << "b is THE GREATEST! HUH!";
+	else if (c > a && c > b)
+		cout << "c is the BIG BOI!";
+}
+// #17 b)
+void greatestIn3TheNestedWay() {
+	int a, b, c;
+	cout << "Enter a b c: ";
+	cin >> a >> b >> c;
+	if (a > b)
+	{
+		if (a > c) cout << "a boi! ";
+		else if (a == c) cout << "a and c too";
+		else cout << "c is the king";
+	}
+	else if (a == b) {
+		if (a > c)
+			cout << "a and b too";
+		else if (a == c)
+			cout << "a b c, saray iko vargay :P";
+		else
+			cout << "c is the king";
+	}
+	else {
+		if (b > c) cout << "b for baray walay log";
+		else if (b == c) cout << " b and c bhai bhai, r..";
+		else cout << "c is the king";
+	}
+}
+// #17 c)
+// ---------------------------------
+// did'nt understand the statement 
+// if you have solution contact me :D
+// ----------------------------------
 // #18
+void greatestIn4() {
+	int a, b, c, d;
+	cout << "Suitable way. just tells the greatest. doesn't tell if he has a bhai :P";
+	cout << "Enter a b c d: ";
+	cin >> a >> b >> c >> d;
+	if (a > b && a > c && a > d) cout << "a for ali bhai";
+	else if (b > a && b > c && b > d) cout << "b for bara bhai";
+	else if (c > a && c > b && c > d) cout << "c for chota but it's bara this time";
+	else if (d > a && d > c && d > b) cout << "d for dado is the best!";
+}
 // #19
+void gradingSystem() {
+	cout << "Trust me, this one is better than LMS! :P";
+	int marks;
+	cout << "Enter user marks: ";
+	cin >> marks;
+	if (!(marks > 0 || marks < 100)) {
+		cout << "...mazak ay?";
+	}
+	else {
+		// valid number idhar ayein gay
+		if (marks < 60) {
+			cout << "bhai fail hai tu, F deta may tujhe. parh liya kar nikamay";
+		}
+		else if (marks < 70 && marks >= 60) {
+			cout << "papu pass hogya. D grade hai bhai. royein naw. mene tujhe pass kar diya hai";
+		}
+		else if (marks < 80 && marks >= 70) {
+			cout << "jani tu great hia. C grade agya";
+		}
+		else if (marks < 90 && marks >= 80) {
+			cout << "tu cheating nahi karata :(. B grade tera";
+		}
+		else if (marks >= 90) {
+			cout << "topper log. A grade. ";
+		}
+	}
+}
 // #20
 void EvenOrOdd() {
 	int number;
@@ -389,6 +596,8 @@ int main()
 {
 	cout << "Call any of function above main() to run. "
 		<< endl
-		<< "Have a good day :)";
+		<< "Have a good day :)"
+		<< endl;
+	greatestIn3TheNestedWay();
 }
 
